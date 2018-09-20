68021321faa47ac13b0233ab31665c0aa6df67b5
Gaon Kim <gaon.kim.dev@gmail.com>
2018-09-20 22:24:04 +0900

    4. 완전 탐색 0

---
+++ Session 6/gaonK/2-4/10973.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N;
+    cin >> N;
+    vector<int> v(N);
+
+    for (int i = 0; i < N; i++) {
+        cin >> v[i];
+    }
+
+    if (prev_permutation(v.begin(), v.end())) {
+        for (int i = 0; i < N; i++) {
+            cout << v[i] << ' ';
+        }
+        cout << '\n';
+    } else {
+        cout << "-1\n";
+    }
+
+    return 0;
+}
